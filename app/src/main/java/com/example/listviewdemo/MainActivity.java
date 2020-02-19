1448a97497a696659d7a35c1004876b9bcb7ed0b
Dell <mollanoman2019@gmail.com>
2020-02-19 17:33:04 +0600

    ListView v-2

--- app/src/main/java/com/example/listviewdemo/MainActivity.java
+++ app/src/main/java/com/example/listviewdemo/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.listviewdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -15,11 +17,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         listView=findViewById(R.id.listViewId);
+
         String[]countryNames=getResources().getStringArray(R.array.country_list);
 
         ArrayAdapter<String>arrayAdapter=new ArrayAdapter<String>(MainActivity.this,R.layout.sample,R.id.textViewId,countryNames);
         listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String name=listView.getItemAtPosition(i).toString();
+                Toast.makeText(MainActivity.this,name+" "+i, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
 
 
     }
+
 }
